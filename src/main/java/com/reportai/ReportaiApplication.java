042e4a69f9ae4ac23208d2db608b1c0bd9e06783
jowgaze <jonatha.biomon.hp@gmail.com>
2026-01-19 16:21:24 -0300

    primeiro commit

---
+++ src/main/java/com/reportai/ReportaiApplication.java
@@ -0,0 +1,13 @@
+package com.reportai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reportai/ReportaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reportai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
